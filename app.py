807d0d4c250ecc123397eca2f47a797ec14182fe
Tommy Dougiamas <tom@digitalnook.net>
2021-09-25 20:47:22 +0800

    init

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/how_to_connect/")
+def how_to_connect():
+    return render_template("how_to_connect.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run()
